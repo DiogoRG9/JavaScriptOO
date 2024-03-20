72c29974386dec1c4568a9ff7c5882bd265bfcf5
Diogo Roque Gaiato <diogogaiato9@gmail.com>
2024-03-19 22:22:47 -0300

    Declaração da Classe Forma

---
+++ 01-Declaracao/forma.js
@@ -0,0 +1,15 @@
+class forma{
+    
+    constructor(){
+        this.tipoDeForma = 'Forma Abstrata'
+    }
+
+    imprimeForma(){
+        //alert (`Tipo de forma: ${this.tipoDeForma} - Área: $(this.calculaArea())`)
+        alert(`Tipo de Forma: ${this.tipoDeForma}`);
+    }
+
+    calculaArea(){
+        throw new Error('o método calculaArea() deve ser implementado na subclass.')
+    }
+}
